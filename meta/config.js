1614ec1c6c3a7cc244746f116f273ba92f057af1
Toutain <charles-edouard.toutain@outlook.fr>
2018-10-06 21:43:44 +0200

    [IMP] add text, color

--- meta/config.js
+++ meta/config.js
@@ -7,16 +7,16 @@ module.exports = {
   siteLogo: '/logos/logo-512x512.png', // Logo used for SEO and manifest.
   siteUrl: 'https://reussir-bts-ndrc.netlify.com', // Domain of your website without pathPrefix.
   pathPrefix: '/', // Prefixes all links. For cases when deployed to example.github.io/gatsby-starter-business/.
-  siteDescription: 'Leverage Gatsby Business Starter for your Business.', // Website description used for RSS feeds/meta description tag.
+  siteDescription: 'Réussir votre BTS négociation digitalisation relation client avec reussir votre bts NDRC ', // Website description used for RSS feeds/meta description tag.
   siteRss: '/rss.xml',
   siteFBAppID: '', // FB Application ID for using app insights
   googleTagManagerID: '', // GTM tracking ID.
   disqusShortname: 'gatsby-business-starter', // Disqus shortname.
   userName: 'Vaibhav Sharma',
   userTwitter: 'vaibhaved',
-  userLocation: 'Delhi NCR, India',
-  userDescription: '',
-  copyright: 'Copyright © Gatsby Starter Business 2018. All Rights Reserved.', // Copyright string for the footer of the website and RSS feed.
-  themeColor: '#00d1b2', // Used for setting manifest and progress theme colors.
+  userLocation: 'Basse-Normandie France.',
+  userDescription: 'Charles-Edouard Toutain Développeur Web full stack.',
+  copyright: 'Copyright © Charles-Edouard Toutain.', // Copyright string for the footer of the website and RSS feed.
+  themeColor: '#fff', // Used for setting manifest and progress theme colors.
   backgroundColor: '#ffffff', // Used for setting manifest background color.
 }
